ee2e813dcb3d21a4a10344282ef04cae11b0e30c
Alita <89002518+alita248@users.noreply.github.com>
2023-04-12 20:01:12 +0200

    test artifact

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 print('Hello world!')
-blabla
-print('new line test')
+
+file = 'test.tst'
+
+with open(file, mode='w') as f:
+    f.write('hello there \n')
+    f.write('done \n')
+
 if __name__ == '__main__':
     print('Done') 
